21ea020cad37e6bbb6d9a05afb8d6b4a35b6a04f
johnkramer1996 <vitaliczinoviev@gmail.com>
2022-02-14 20:46:36 +0200

    day 21 - added tab students - sort

--- src/components/CoursesTabs/CoursesTabsStudents.jsx
+++ src/components/CoursesTabs/CoursesTabsStudents.jsx
@@ -3,25 +3,28 @@ import { useSelector } from 'react-redux'
 import { coursesSelectors } from 'store/selectors'
 import CoursesTabsStudentsItem from './CoursesTabsStudentsItem'
 import { ReactComponent as ArrowChangeSvg } from 'svg/arrow-change.svg'
+import { declOfNum, getDeclOfArray, isActiveClass } from 'utils'
 
 const CoursesTabsStudents = () => {
    const course = useSelector(coursesSelectors.getCourse)
    const users = course.users || []
-   const [sort, setSort] = useState(false)
+   const [isSort, setIsSort] = useState(false)
 
-   console.log(course)
+   const sortedUsers = isSort ? [...users].reverse() : [...users]
 
    return (
       <div className='students-tab'>
          <div className='students-tab__top'>
-            <div className='students-tab__title'>11 учеников</div>
-            <button className='students-tab__btn'>
-               <span>Сначала новые</span>
+            <div className='students-tab__title'>
+               {sortedUsers.length} {declOfNum(sortedUsers.length, getDeclOfArray['users'])}
+            </div>
+            <button className={`students-tab__btn${isActiveClass(isSort, 'students-tab__btn--active')}`} onClick={() => setIsSort(!isSort)}>
+               <span>Сначала {isSort ? 'новые' : 'старые'}</span>
                <ArrowChangeSvg />
             </button>
          </div>
          <div className='students-tab__items'>
-            {users.map((props, index) => (
+            {sortedUsers.map((props, index) => (
                <CoursesTabsStudentsItem key={props.id} {...props} />
             ))}
          </div>
